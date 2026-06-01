ce70e7655bb77b5df6802578ae9f46791a35bbc2
wxlai <wxlai128@gmail.com>
2026-06-01 17:33:28 +0800

    完成Project 1 Knights

--- project1-knights/puzzle.py
+++ project1-knights/puzzle.py
@@ -9,24 +9,67 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
+AEitherKnightOrKnave = And(
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+)
+
+BEitherKnightOrKnave = And(
+    Or(BKnight, BKnave),
+    Not(And(BKnight, BKnave)),
+)
+
+CEitherKnightOrKnave = And(
+    Or(CKnight, CKnave),
+    Not(And(CKnight, CKnave)),
+)
+
 # Puzzle 0
 # A says "I am both a knight and a knave."
+ASentence0 = And(AKnight, AKnave)
 knowledge0 = And(
-    # TODO
+    AEitherKnightOrKnave,
+    Or(
+        And(AKnight, ASentence0),
+        And(AKnave, Not(ASentence0)),
+    ),
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
+ASentence1 = And(AKnave, BKnave)
 knowledge1 = And(
-    # TODO
+    AEitherKnightOrKnave,
+    BEitherKnightOrKnave,
+    Or(
+        And(AKnight, ASentence1),
+        And(AKnave, Not(ASentence1)),
+    ),
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
+ASentence2 = Or(
+    And(AKnight, BKnight),
+    And(AKnave, BKnave),
+)
+BSentence2 = Or(
+    And(AKnight, BKnave),
+    And(AKnave, BKnight),
+)
 knowledge2 = And(
-    # TODO
+    AEitherKnightOrKnave,
+    BEitherKnightOrKnave,
+    Or(
+        And(AKnight, ASentence2),
+        And(AKnave, Not(ASentence2)),
+    ),
+    Or(
+        And(BKnight, BSentence2),
+        And(BKnave, Not(BSentence2)),
+    ),
 )
 
 # Puzzle 3
@@ -34,8 +77,32 @@ knowledge2 = And(
 # B says "A said 'I am a knave'."
 # B says "C is a knave."
 # C says "A is a knight."
+ASaidKnave = Symbol("A said 'I am a knave'.")
+ASaidKnight = Symbol("A said 'I am a knight'.")
+ASentence3 = Or(
+    And(ASaidKnave, AKnave),
+    And(ASaidKnight, AKnight),
+)
+CSentence3 = AKnight
+
 knowledge3 = And(
-    # TODO
+    AEitherKnightOrKnave,
+    BEitherKnightOrKnave,
+    CEitherKnightOrKnave,
+    Or(ASaidKnave, ASaidKnight),
+    Not(And(ASaidKnave, ASaidKnight)),
+    Or(
+        And(AKnight, ASentence3),
+        And(AKnave, Not(ASentence3)),
+    ),
+    Or(
+        And(BKnight, ASaidKnave, CKnave),
+        And(BKnave, Not(ASaidKnave), Not(CKnave)),
+    ),
+    Or(
+        And(CKnight, CSentence3),
+        And(CKnave, Not(CSentence3)),
+    ),
 )
 
 
